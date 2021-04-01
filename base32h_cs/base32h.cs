5cfa1bb428a1a212c3c9dbb2bcea45d766d0ab6d
JamesFloyd-Pen <Commanderpip@gmail.com>
2021-04-01 15:32:40 -0400

    Encode Built

--- base32h_cs/base32h.cs
+++ base32h_cs/base32h.cs
@@ -1,12 +1,83 @@
 ﻿using System;
+using System.Collections;
 
 namespace base32h_cs
 {
     class base32h
     {
+        public string[] digits = {
+            "0Oo",
+            "1Ii",
+            "2",
+            "3",
+            "4",
+            "5Ss",
+            "6",
+            "7",
+            "8",
+            "9",
+            "Aa",
+            "Bb",
+            "Cc",
+            "Dd",
+            "Ee",
+            "Ff",
+            "Gg",
+            "Hh",
+            "Jj",
+            "Kk",
+            "Ll",
+            "Mn",
+            "Nn",
+            "Pp",
+            "Qq",
+            "Rr",
+            "Tt",
+            "VvUu",
+            "Ww",
+            "Xx",
+            "Yy",
+            "Zz"
+        };
+
+        public ArrayList encode(long input){
+
+            ArrayList output = new ArrayList();
+
+
+            if (input == 0)
+            {
+                output.Add("0");
+                return output;
+            }
+
+            while(input > 0)
+            {
+                long temp = input % 32;
+                int indexPointer = (int)temp;
+                output.Insert(0, digits[indexPointer % 32].Substring(0, 1));
+                input = input/32;
+            }
+
+            return output;
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            base32h myBase = new base32h();
+            //System.Console.WriteLine("Hello World");
+            var x = myBase.encode(17854910);
+            myBase.printArrayList(x);
+
         }
+
+        void printArrayList(ArrayList x)
+        {
+            foreach (Object obj in x)
+                Console.Write(" {0}", obj);
+            Console.WriteLine();
+        }
+
     }
 }
